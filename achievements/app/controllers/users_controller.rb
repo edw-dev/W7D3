2e1d9eccdb905681bb63f4713a0db6423f05afcd
Ed <eddieisabsnot+git@gmail.com>
2021-11-18 06:50:15 -0500

    index and show rspec

--- achievements/app/controllers/users_controller.rb
+++ achievements/app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+        render :index
+    end
+    def show 
+        @user = User.find(params[:id])
+        render :show
+    end
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to user_url(@user)
+        else
+            flash[:errors]=@user.errors.full_messages
+            
+            render :new
+        end
+    end
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
--- achievements/spec/controllers/users_spec.rb
+++ achievements/spec/controllers/users_spec.rb
@@ -1,7 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  subject(:bob){
+    User.create!(
+      username: 'Bob',
+      password: 'password'
+    )
+  }
+
   describe "GET /index" do
-    
+    it "renders index" do
+      get :index
+      expect(response).to render_template("index")
+    end
   end
+  describe "GET /show" do
+    it "shows" do
+      get :show, params:{id:bob.id}
+      expect(response).to render_template("show")
+    end
+  end
+
+  describe "POST /create" do
+    context "valid params" do
+      it "redirects to user: show" do
+        post :create, params: {
+          user: {
+            username: "bob",
+            password: "passer"
+          }
+        }
+        expect(response).to redirect_to(user_url(User.last))
+      end
+    end
+    context "INvalid params" do
+      it "redirects to user: new" do
+        post :create, params: {
+          user: {
+            username: "bob",
+            password: "yer"
+          }
+        }
+        expect(response).to render_template("new")
+      end
+    end
+  end
+
 end
